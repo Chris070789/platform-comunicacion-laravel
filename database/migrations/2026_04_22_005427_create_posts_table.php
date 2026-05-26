8ff22fec1e7f2eabe20ee26b5534dfe06d0ef086
Chris070789 <crisrolon89@hotmail.com>
2026-05-25 21:30:38 -0300

    act25052026docker
    Conexion y despliegue a docker

--- database/migrations/2026_04_22_005427_create_posts_table.php
+++ database/migrations/2026_04_22_005427_create_posts_table.php
@@ -14,8 +14,8 @@ return new class extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->foreignId('topic_id')->constrained()->onDelete('cascade');
-            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->text('content');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
